4de26ece7ffb18289aab5b88e033330ff88ea827
azxyqian <azxiyuanqian@outlook.com>
2025-11-07 16:17:20 +0800

    Delete node/build directory

--- node/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lxy/rosplace/node
-# Build directory: /home/lxy/rosplace/node/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
